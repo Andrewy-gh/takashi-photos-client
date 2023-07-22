cb32d21a81c877171e3020cd6ee6dd992701f6b0
Andy-git985 <andy.yu617@gmail.com>
2023-07-21 21:54:33 -0400

    change baseurl

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL:
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:3001'
-      : 'https://takashi-photos-api.onrender.com',
-
+  baseURL: 'https://takashi-photos.fly.dev/',
   withCredentials: true,
 });
 
