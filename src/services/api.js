a53f5d1eef760888461d4258652c21569e1b6140
Andy-git985 <andy.yu617@gmail.com>
2023-07-21 22:14:55 -0400

    change baseurl

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://takashi-photos.fly.dev/',
+  baseURL: 'https://takashi-photos.fly.dev',
   withCredentials: true,
 });
 
